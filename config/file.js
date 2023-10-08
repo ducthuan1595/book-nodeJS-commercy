212ee58b74b72eb9758d574d98c71d1d7f173df9
thuantruong <hi.tim.gi.the@gmail.com>
2023-10-08 17:25:06 +0700

    image1

--- config/file.js
+++ config/file.js
@@ -24,7 +24,9 @@ exports.deleteFile = (images) => {
 exports.sendImage = (req, res) => {
   const imageUrl = req.params.imageUrl;
   // const p1 = path.join(__dirname, "..", "data", "images", imageUrl);
-  const uploadPath = path.join(__dirname, "..", p, imageUrl);
+  // const uploadPath = path.join(__dirname, "..", p, imageUrl);
+  const uploadPath = path.join("/tmp", imageUrl);
+  console.log(uploadPath);
   res.set("Content-Type", "image/png");
   res.sendFile(uploadPath);
 };
